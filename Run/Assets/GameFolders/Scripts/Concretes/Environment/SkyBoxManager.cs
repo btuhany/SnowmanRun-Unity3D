54c9e1e7482259de28c3deba6580fc8127eff992
Batuhan YİĞİT <114235184+btuhany@users.noreply.github.com>
2023-02-01 22:10:28 +0300

    prefab level design

--- Run/Assets/GameFolders/Scripts/Concretes/Environment/SkyBoxManager.cs
+++ Run/Assets/GameFolders/Scripts/Concretes/Environment/SkyBoxManager.cs
@@ -5,20 +5,24 @@ using UnityEngine;
 public class SkyBoxManager : MonoBehaviour
 {
 
-    [SerializeField] float _skyRotationSpeed;
+    [SerializeField] float _dayNightCycleSpeed;
     float _transitionValue;
     bool IsNight;
 
+    private void Start()
+    {
+        RenderSettings.skybox.SetFloat("_CubemapTransition", _transitionValue);
+    }
     private void Update()
     {
         if (IsNight)
         {
-            _transitionValue -= Time.deltaTime * _skyRotationSpeed;
+            _transitionValue -= Time.deltaTime * _dayNightCycleSpeed;
             if(_transitionValue <= 0) { IsNight= false; }
         }
         else
         {
-            _transitionValue += Time.deltaTime * _skyRotationSpeed;
+            _transitionValue += Time.deltaTime * _dayNightCycleSpeed;
             if (_transitionValue >= 1) { IsNight = true; }
         }
 
